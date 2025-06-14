97fe1afbe8e6f6f27dc3cf0ee67f515116e85792
Rajdeep Rudra <121824448+rajdeeprudra@users.noreply.github.com>
2025-06-14 13:44:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext"; // Correct import
 import Routes from "./routs"; // Correct import for your routes
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://gossip-backend-wv5l.onrender.com";
   axios.defaults.withCredentials = true;
  
   return (
