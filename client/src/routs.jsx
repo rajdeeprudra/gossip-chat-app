486e0524e2d078fe9be21aaa55daacf5f3220975
Rajdeep Rudra <121824448+rajdeeprudra@users.noreply.github.com>
2025-06-14 13:23:07 +0530

    Update routs.jsx

--- client/src/routs.jsx
+++ client/src/routs.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
-import { UserContext } from "./userContext";
+import { UserContext } from "./UserContext";
 import Chat from "./chat";
 
 export default function Routes(){
